20275ee93e6a8be6b780a0a7896c3f64c1233f2a
AntonyKLINGER <klingerantony@gmail.com>
2024-02-08 16:04:30 +0100

    header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-  
+      <Head>
+        <title>Les Rencontres des Territoires Engagés</title>
+      </Head>
+      
     </>
   )
 }
