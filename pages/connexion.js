bcad2692a00f98bb370d3c13edcce431b44e9628
AntonyKLINGER <klingerantony@gmail.com>
2024-02-09 14:43:53 +0100

    maj

--- pages/connexion.js
+++ pages/connexion.js
@@ -5,7 +5,33 @@ export default function Login(){
     return (
         <>
             <div className={styles.Login}>
-                 
+                <div className="boxed">
+                    <div className="flex justicenter">
+                        <div className={styles.Logbox}>
+                            <h1>Se connecter aux rencontres</h1>
+                            <div className={styles.Box}>
+                                <div className="mBot20">
+                                    <input type="mail" className="input-mail" placeholder="Adresse e-mail..." />
+                                </div>
+                                <div className="mBot20">
+                                    <input type="password" className="input-text" placeholder="Mot de passe" />
+                                </div>
+                                <div className="flex flex-end gap10">
+                                    <button className="btn__normal btn__light">
+                                        Créer un compte
+                                    </button>
+                                    <button className="btn__normal btn__dark">
+                                        Se connecter
+                                    </button>
+                                </div>
+                                <div className="alignright">
+                                    <Link className="link mTop15" href="/">Mot de passe oublié ?</Link>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
