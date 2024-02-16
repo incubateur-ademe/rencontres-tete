c1478b5769e4e8e5288dc01cef5a001470a829bd
AntonyKLINGER <klingerantony@gmail.com>
2024-02-16 14:42:32 +0100

    maj nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import Head from 'next/head'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
@@ -5,6 +8,13 @@ import '@/styles/responsify-min.css'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+
+  NProgress.configure({ showSpinner: false });
+
+  Router.events.on('routeChangeStart', () => NProgress.start());
+  Router.events.on('routeChangeComplete', () => NProgress.done());
+  Router.events.on('routeChangeError', () => NProgress.done());
+
   return (
     <>
       <Head>
