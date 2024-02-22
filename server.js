391ddd7c429a276fd286a33789539750aee7b0c2
AntonyKLINGER <klingerantony@gmail.com>
2024-02-22 15:02:48 +0100

    maj server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  server.use("/uploads", express.static(__dirname + "/uploads"));
+  server.use("/uploads", express.static(__dirname + "/tmp/uploads"));
 
   server.all('*', (req, res) => {
       return handle(req, res)
