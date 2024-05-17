a9344bebc939abf1d0f7194311e694ea0cdcf76f
AntonyKLINGER <klingerantony@gmail.com>
2024-05-17 10:40:31 +0200

    maj emails

---
+++ pages/api/emails/welcome.js
@@ -0,0 +1,60 @@
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const path = require('path');
+import https from 'https';
+
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false, // ATTENTION: Ne pas utiliser en production pour des raisons de sécurité
+});
+
+export default async function handler(req, res) {
+
+  //const { id, lang } = req.body
+
+  // Configuration de Nodemailer
+  const transporter = nodemailer.createTransport({
+    host: 'smtp-relay.brevo.com', // Remplacez par l'adresse de votre serveur SMTP
+    port: 587, // Port SMTP standard, ajustez selon vos besoins
+    secure: false, // Mettez à true pour le port 465, ou false pour les autres ports
+    auth: {
+      user: 'contact@territoiresentransitions.fr', // Remplacez par votre nom d'utilisateur SMTP
+      pass: '' // Remplacez par votre mot de passe SMTP
+    },
+    tls: {rejectUnauthorized: false}
+  });
+
+
+  // Configuration de nodemailer-express-handlebars
+  transporter.use('compile', hbs({
+    viewEngine: {
+      extName: '.hbs',
+      partialsDir: path.resolve('./views/emails'),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve('./views/emails'),
+    extName: '.hbs',
+  }));
+
+  // Données pour le template
+  const mailOptions = {
+    from: '"ADEME" <contact@territoiresentransitions.fr>',
+    //to: data.email,
+    to: 'antony@innov-events.fr',
+    subject: "Bienvenue sur la plateforme des Rencontres Territoire Engagé Transition Ecologique !",
+    template: 'welcome',
+    context: {
+  
+    }
+  };
+
+  // Envoi de l'e-mail
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.json(error)
+      // return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    res.status(200).json({ status: 'sended' })
+  });
+
+}
