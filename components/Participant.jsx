3933546632d3b6859c110f9045e7d807dc8dc916
AntonyKLINGER <klingerantony@gmail.com>
2024-06-03 09:42:50 +0200

    maj 03/06 >> besoins + ajout titre par jour

--- components/Participant.jsx
+++ components/Participant.jsx
@@ -44,10 +44,12 @@ export default function Participant({ data, setActions, session }){
                     <div className="w22"><span className={styles.Label}>Fonction</span>{data.fonction}</div>
                     <div className="w30"><span className={styles.Label}>Type de fonction</span>{data.typeFonction}</div>
                     <div className="w20"><span className={styles.Label}>Ville</span>{data.ville ? data.ville : '-'}</div>
+                    <div className="w22"><span className={styles.Label}>Région</span>{data.region}</div>
                     <div className="w22"><span className={styles.Label}>Transport</span>{data.transport}</div>
-                    <div className="w22"><span className={styles.Label}>Repas</span>{data.repas ? 'J1 : Oui' : 'J1 : Non'}{data.session.metasSession.nombreJours == 2 ? data.repas2 ? ' / J2 : Oui' : ' / J2 : Non' : ''}{data.regime ? ` (${data.regime})` : ''}</div>
+                    <div className="w20"><span className={styles.Label}>Repas</span>{data.repas ? 'J1 : Oui' : 'J1 : Non'}{data.session.metasSession.nombreJours == 2 ? data.repas2 ? ' / J2 : Oui' : ' / J2 : Non' : ''}{data.regime ? ` (${data.regime})` : ''}</div>
                     <div className="w30"><span className={styles.Label}>Besoin spécifique</span>{data.besoins != null ? data.besoins : '-'}</div>
-                    <div className="w20"><span className={styles.Label}>Présence</span>{data.deleted ? 'Annulée' : data.days ? 'Complète' : '1/2'}</div>
+                    <div className="w20 mTop10"><span className={styles.Label}>Présence</span>{data.deleted ? 'Annulée' : data.days ? 'Complète' : '1/2'}</div>
+                    
                 </div>
             </div>
 
