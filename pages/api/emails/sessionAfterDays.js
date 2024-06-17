8d05be2ef39c5fe0746200808554206581c303bf
Antony Klinger <40170587+AntonyKLINGER@users.noreply.github.com>
2024-06-17 10:55:48 +0200

    Update from repo a (#6)
    * maj comptes admin + dr
    
    * mise à jour 29/05
    
    * maj
    
    * maj 29/05
    
    * maj email style
    
    * mise à jour 29/05
    
    * maj comptes admin + dr (#1)
    
    * maj 03/06 >> besoins + ajout titre par jour
    
    * maj visuel session
    
    * maj comptes DR
    
    * maj session programme + dr
    
    * maj emails
    
    * Update 3/06 - 14h14 (#4) (#5)
    
    * maj comptes admin + dr
    
    * mise à jour 29/05
    
    * maj
    
    * maj 29/05
    
    * maj email style
    
    * mise à jour 29/05
    
    * maj comptes admin + dr (#1)
    
    * maj 03/06 >> besoins + ajout titre par jour
    
    * maj visuel session
    
    * maj comptes DR
    
    * maj session programme + dr
    
    ---------
    
    Co-authored-by: lollybet_antony <antony@s-media.fr>
    
    ---------
    
    Co-authored-by: lollybet_antony <antony@s-media.fr>

--- pages/api/emails/sessionAfterDays.js
+++ pages/api/emails/sessionAfterDays.js
@@ -1,14 +1,9 @@
-const nodemailer = require('nodemailer');
-const hbs = require('nodemailer-express-handlebars');
-const path = require('path');
-
+import nodemailer from 'nodemailer';
+import hbs from 'nodemailer-express-handlebars';
+import path from 'path';
 
 export default async function handler(req, res) {
-
-  const { email, password, type, regions, modules } = req.body
-
-  const formattedRegions = regions.join(', ');
-  const formattedModules = modules.join(', ');
+  const { prenom, email } = req.body;
 
   const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
@@ -18,7 +13,7 @@ export default async function handler(req, res) {
       user: 'contact@territoiresentransitions.fr',
       pass: process.env.BREVO_KEY
     },
-    tls: {rejectUnauthorized: false}
+    tls: { rejectUnauthorized: false }
   });
 
   transporter.use('compile', hbs({
@@ -34,25 +29,20 @@ export default async function handler(req, res) {
   const mailOptions = {
     from: '"ADEME" <contact@territoiresentransitions.fr>',
     to: email,
-    subject: "Votre nouveau compte sur les Rencontres Territoire Engagé Transition écologique !",
-    template: type == 'DR' ? 'newaccountdr' : 'newaccount',
+    subject: "Merci pour votre présence à la Rencontre Territoire Engagé Transition Ecologique",
+    template: 'session_after_day',
     context: {
-        password: password,
-        type: type,
-        email: email,
-        regions: formattedRegions,
-        modules: formattedModules,
-        siteUrl: process.env.WEBSITE_URL,
+      prenom: prenom,
+      siteUrl: process.env.WEBSITE_URL,
     }
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      res.json(error)
-      // return console.log(error);
+      res.json(error);
+      return console.log(error);
     }
     console.log('Message sent: %s', info.messageId);
-    res.status(200).json({ status: 'sended' })
+    res.status(200).json({ status: 'sended' });
   });
-
 }
---
+++ pages/api/workflow/workflowAfter.js
@@ -0,0 +1,75 @@
+import prisma from '@/prisma'
+import fetch from 'node-fetch'
+
+export default async function handle(req, res) {
+    if (req.method !== 'GET') {
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+        return;
+    }
+    
+    try {
+        const today = new Date();
+        const oneDayBefore = new Date(today);
+        oneDayBefore.setDate(today.getDate() - 1);
+
+        const pastSessions = await prisma.session.findMany({
+            where: {
+                dateFin: {
+                    equals: oneDayBefore,
+                },
+            },
+            include: {
+                registrations: {
+                    include: {
+                        user: true,
+                    },
+                },
+                metasSession: true,
+                module: true,
+            },
+        });
+
+        for (const session of pastSessions) {
+            const firstProgramme = session.metasSession.programmeSession[0];
+            let firstDayStartTime;
+
+            if (firstProgramme.horaires.includes('Jour')) {
+                firstDayStartTime = firstProgramme.horaires.split(' : ')[1].split(' - ')[0];
+            } else {
+                firstDayStartTime = firstProgramme.horaires.split(' - ')[0].trim();
+            }
+
+            const dateDebut = new Date(session.dateDebut);
+            const formattedDateDebut = dateDebut.toLocaleDateString('fr-FR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+            });
+
+            for (const registration of session.registrations) {
+                const userData = registration.user;
+
+                const emailResponse = await fetch(`${process.env.WEBSITE_URL}/api/emails/sessionAfterDays`, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        prenom: userData.prenom,
+                        email: userData.mail,
+                    })
+                });
+
+                if (!emailResponse.ok) {
+                    throw new Error(`Email request failed with status ${emailResponse.status}`);
+                }
+            }
+        }
+
+        res.status(200).json({ message: "Emails sent successfully" });
+    } catch (error) {
+        console.error("Error fetching past sessions or sending emails: ", error);
+        res.status(500).json({ error: `Impossible de récupérer les enregistrements ou d'envoyer les emails : ${error.message}` });
+    }
+}
