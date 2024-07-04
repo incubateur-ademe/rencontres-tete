07dc7e5b8880a3c90699da8f22a2d879ad644359
AntonyKLINGER <klingerantony@gmail.com>
2024-07-04 14:38:57 +0200

    maj email after 1 day

---
+++ .github/workflows/scripts/fetchDataDayAfter.js
@@ -0,0 +1,10 @@
+(async () => {
+    const fetch = await import('node-fetch').then(module => module.default);
+  
+    const res = await fetch(`https://rencontres.territoiresentransitions.fr/api/workflow/workflowAfter`, {
+      method: 'GET'
+    });
+    const data = await res.json();
+    console.log(data);
+  })();
+  
