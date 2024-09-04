c2ac57e446627f50b817978732d98c4705ba1a43
AntonyKLINGER <klingerantony@gmail.com>
2024-09-04 15:05:28 +0200

    maj gets

--- pages/api/emails/gets.js
+++ pages/api/emails/gets.js
@@ -1,9 +1,52 @@
-import nodemailer from 'nodemailer';
-import hbs from 'nodemailer-express-handlebars';
-import path from 'path';
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const path = require('path');
 
 export default async function handler(req, res) {
-  
-    res.status(200).json({ a: process.env.WEBSITE_URL, b: process.env.BREVO_KEY });
+
+  const { prenom, email } = req.body
+
+  res.status(200).json({ prenom: prenom })
+
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp-relay.brevo.com',
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: 'contact@territoiresentransitions.fr',
+//       pass: process.env.BREVO_KEY
+//     },
+//     tls: {rejectUnauthorized: false}
+//   });
+
+//   transporter.use('compile', hbs({
+//     viewEngine: {
+//       extName: '.hbs',
+//       partialsDir: path.resolve('./views/emails'),
+//       defaultLayout: false,
+//     },
+//     viewPath: path.resolve('./views/emails'),
+//     extName: '.hbs',
+//   }));
+
+//   const mailOptions = {
+//     from: '"ADEME" <contact@territoiresentransitions.fr>',
+//     to: email,
+//     subject: "Bienvenue sur la plateforme des Rencontres Territoire Engagé Transition Ecologique !",
+//     template: 'welcome',
+//     context: {
+//         prenom: prenom,
+//         siteUrl: process.env.WEBSITE_URL,
+//     }
+//   };
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       res.json(error)
+//       // return console.log(error);
+//     }
+//     console.log('Message sent: %s', info.messageId);
+//     res.status(200).json({ status: 'sended' })
+//   });
 
 }
