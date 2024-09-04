fa67aee32393fb5043d5ab7aa8702f8c26651dfd
AntonyKLINGER <klingerantony@gmail.com>
2024-09-04 15:49:18 +0200

    maj step 8

--- pages/api/emails/gets.js
+++ pages/api/emails/gets.js
@@ -1,17 +1,8 @@
-const nodemailer = require('nodemailer');
-const hbs = require('nodemailer-express-handlebars');
-const path = require('path');
+import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
   try {
-    // Extraction des données du corps de la requête
-    const { prenom, email } = req.body;
-
-    if (!prenom || !email) {
-      return res.status(400).json({ error: 'Données manquantes' });
-    }
-
-    // Configuration du transporteur Nodemailer
+    // Configuration du transporteur SMTP
     const transporter = nodemailer.createTransport({
       host: 'smtp-relay.brevo.com',
       port: 587,
@@ -23,41 +14,18 @@ export default async function handler(req, res) {
       tls: { rejectUnauthorized: false }
     });
 
-    // Configuration de Handlebars pour les templates
-    transporter.use('compile', hbs({
-      viewEngine: {
-        extName: '.hbs',
-        partialsDir: path.resolve('./views/emails'),
-        defaultLayout: false,
-      },
-      viewPath: path.resolve('./views/emails'),
-      extName: '.hbs',
-    }));
-
-    // Options d'e-mail
-    const mailOptions = {
-      from: '"ADEME" <contact@territoiresentransitions.fr>',
-      to: email,
-      subject: "Bienvenue sur la plateforme des Rencontres Territoire Engagé Transition Ecologique !",
-      template: 'welcome',
-      context: {
-        prenom: prenom,
-        siteUrl: process.env.WEBSITE_URL,
+    // Test de la connexion SMTP
+    transporter.verify(function(error, success) {
+      if (error) {
+        console.log('Erreur de connexion SMTP:', error);
+        return res.status(500).json({ status: 'error', message: 'Connexion au serveur SMTP échouée', details: error.message });
+      } else {
+        console.log('Connexion au serveur SMTP réussie');
+        return res.status(200).json({ status: 'success', message: 'Connexion au serveur SMTP réussie' });
       }
-    };
-
-    // Tentative d'envoi de l'e-mail
-    try {
-      const info = await transporter.sendMail(mailOptions); // Utilisation d'await
-      console.log('Message sent: %s', info.messageId);
-      return res.status(200).json({ status: 'sended', messageId: info.messageId });
-    } catch (error) {
-      console.error('Erreur lors de l\'envoi de l\'e-mail:', error);
-      return res.status(500).json({ error: 'Erreur lors de l\'envoi de l\'e-mail', details: error.message });
-    }
-
+    });
   } catch (error) {
-    console.error('Erreur générale:', error);
-    return res.status(500).json({ error: 'Erreur générale', details: error.message });
+    console.error('Erreur:', error);
+    res.status(500).json({ status: 'error', message: 'Erreur lors de la tentative de connexion', details: error.message });
   }
 }
