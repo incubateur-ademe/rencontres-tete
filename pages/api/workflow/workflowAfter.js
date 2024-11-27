1b21618a2ddad767d24bd35a3ff7f9e931f33aa3
AntonyKLINGER <klingerantony@gmail.com>
2024-11-27 11:05:24 +0100

    maj workflow

--- pages/api/workflow/workflowAfter.js
+++ pages/api/workflow/workflowAfter.js
@@ -18,18 +18,12 @@ export default async function handle(req, res) {
             where: {
                 dateDebut: {
                     lte: today,
-                    lte: today,
                 },
             },
             include: {
                 registrations: {
                     include: {
                         user: true,
-                    },
-                },
-                accountRegistrations: {
-                    include: {
-                        account: true,
                     },
                 },
                 accountRegistrations: {
